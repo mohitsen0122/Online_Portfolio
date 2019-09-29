ffadb0fcdc260bf451a45226033179cd25f79f44
Mohit Sen <mohitsen0122@gmail.com>
2019-09-29 16:40:47 +0530

    CSS And JS Changes
      Adds CSS and JS Files for Individual Pages

---
+++ js/onlinePortfolio.js
@@ -0,0 +1,10 @@
+function hoverOnImg() {
+	var eleImg = document.getElementById('img1');
+	eleImg.className = "imgPortfolioHover";
+
+}
+function hoverOffImg() {
+	var eleImg = document.getElementById('img1');
+	eleImg.className = "imgPortfolio";
+
+}
